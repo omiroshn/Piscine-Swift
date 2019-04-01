8bcdbd8301085685f13dc7d80b8d0ae7870fe50f
omiroshn <omiroshn@student.unit.ua>
2019-04-01 21:21:46 +0300

    added ex00

---
+++ day00/ex00/ex00/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Lesha Miroshnik on 4/1/19.
+//  Copyright © 2019 Lesha Miroshnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func clickMeButton(_ sender: UIButton) {
+        print("Hello world!")
+    }
+    
+}
+
