61e7c794d2f418cea095d31e1dbb49b85ea461af
omiroshn <omiroshn@student.unit.ua>
2019-04-03 16:23:19 +0300

    added ex00

---
+++ day01/ex00/main.swift
@@ -0,0 +1,10 @@
+let colors = Color.allColors
+let values = Value.allValues
+
+for color in colors {
+    print("\(color) => \(color.rawValue)")
+}
+
+for value in values {
+    print("\(value) => \(value.rawValue)")
+}
